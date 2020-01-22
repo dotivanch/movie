ae3372a7dca145b5976f15663684741037ff3dcd
José Henrique Ivanchechen <j.ivanchechen@cinq.com.br>
2020-01-22 10:24:15 -0300

    fix

--- TVShow.ts
+++ TVShow.ts
@@ -1,6 +1,7 @@
+import Show from './Show/Show';
 import Season from './TVShow/Season';
 
-export class TVShow {
+export class TVShow extends Show {
     seasons: Season[];
     in_production: boolean;
     last_air_date: string;
