de3b31f60394749731d633e87e409d5723f94e5c
José Henrique Ivanchechen <j.ivanchechen@cinq.com.br>
2020-01-22 13:16:07 -0300

    add

--- Show/Show.ts
+++ Show/Show.ts
@@ -1,14 +1,18 @@
 import Genre from './Genre';
+import ProductionCompany from './ProductionCompany';
 
 export default class Show {
+    backdrop_path: string;
+    genres: Genre[];
+    homepage: string;
     id: number;
     title: string;
+    original_language: string;
     overview: string;
     popularity: number;
+    production_companies: ProductionCompany[];
     status: string;
     vote_average: number;
     vote_count: number;
-    genres: Genre[];
     poster_path: string;
-    homepage: string;
 };
--- TVShow.ts
+++ TVShow.ts
@@ -3,8 +3,14 @@ import Season from './TVShow/Season';
 
 export class TVShow extends Show {
     seasons: Season[];
+    episode_run_time: number[];
+    first_air_date: string;
     in_production: boolean;
+    languages: string[];
     last_air_date: string;
     number_of_episodes: number;
     number_of_seasons: number;
+    origin_country: string[];
+    original_name: string;
+    type: string;
 };
